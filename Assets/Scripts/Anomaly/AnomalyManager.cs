9a6b63238c8753e2533ffd73b1be7d1e5c057314
Dar <dardaniel96@gmail.com>
2025-11-17 15:11:43 +0100

    sound bites

--- Assets/Scripts/Anomaly/AnomalyManager.cs
+++ Assets/Scripts/Anomaly/AnomalyManager.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 public class AnomalyManager : MonoBehaviour
 {
-    [SerializeField] private float anomalyChance = 0.6f; // 60% chance of an anomaly
+    [SerializeField] private float anomalyChance = 0.9f; // 60% chance of an anomaly
     [SerializeField] private float obviousnessChance = 0.2f; // 20% obvious 80% not 
     private static List<Anomaly> allAnomalies; 
     private List<Anomaly> availableAnomalies;
